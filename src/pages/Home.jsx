1275bb11a8dc00e01db7af71ac5cd8d86396bfea
halimaxo <halimakshuchi@gmail.com>
2023-07-05 19:16:39 -0500

    Making the Home page and Dashboard

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <section>
+            <div>
+                <p>
+                    Home
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Home
